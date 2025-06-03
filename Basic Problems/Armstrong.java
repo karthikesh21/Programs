fa04c5a577e0478f11d7cd9ce17648f0ae323cec
karthikesh21 <karthikes004h@gmail.com>
2025-06-03 17:28:35 +0530

    Update Armstrong.java

--- Basic Problems/Armstrong.java
+++ Basic Problems/Armstrong.java
@@ -1,19 +1,22 @@
+import java.io.*;
 import java.util.*;
-public class Armstrong{
-    public static void main(String[] kar){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter a number:");
-    int num=sc.nextInt();
-    int org=num,sum=0;
-    while(num>0){
-        int digit=num%10;
-        sum+=Math.pow(digit, 3);
-        num/=10;
+
+public class Solution {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner (System.in);
+        int num=sc.nextInt();
+        int org=num,sum=0;
+        int digits=String.valueOf(num).length();
+        while(num>0){
+            int digit=num%10;
+            sum+=(int)(Math.pow(digit,digits));
+            num/=10;
+        }
+        if(sum==org){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
     }
-    if(sum==org){
-        System.out.println(org + " is an Armstrong number.");
-    } else {
-        System.out.println(org + " is not an Armstrong number.");
-    }
-}
 }
