06353b4b775b684498a3b2d31ad3c0f4502b036c
karthikesh21 <karthikes004h@gmail.com>
2025-06-03 12:57:02 +0530

    Update ASCII Value.java

--- Basic Problems/ASCII Value.java
+++ Basic Problems/ASCII Value.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class DectoBinary{
+class ASCII{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Ascii value :");
